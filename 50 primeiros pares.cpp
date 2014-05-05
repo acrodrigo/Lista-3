43dadb63958ece2ccbf7ae17df3535ee15d1b761
acrodrigo <alencarcosta3@gmail.com>
2014-05-05 11:01:20 -0300

    Update 50 primeiros pares.cpp

--- 50 primeiros pares.cpp
+++ 50 primeiros pares.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<conio.h>
 	int main(){
 		int i, a, res, c;
 		i=a=res=0;
@@ -8,12 +8,12 @@
 				{
 				a = a +2;
 				res += a;
-				printf("Esses são os ciquenta primeiros pares %d\n", a);
+				printf("Esses sï¿½o os ciquenta primeiros pares %d\n", a);
 			/*	printf("%d\n", res);*/                                                 
 				i++;
 				}
 				
 				c = res;
-				printf ("A soma dos 50 primeiros pares é %d\n", c);
+				printf ("A soma dos 50 primeiros pares ï¿½ %d\n", c);
 	}
 	
